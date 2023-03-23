31c0045f59176b723078ba9a3cc900272eaee531
endiesworld <endiesworld@gmail.com>
2023-03-23 03:24:18 +0100

    feat: Added requirements.txt file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # python-chatGPT
+
+## Development
+
+### 1. Create and update local .env
+
+``` Bash
+cp .env.example .env
+```
+
+### 2. Setup virtual env and install dependencies
+``` Bash
+> python3.9 -m venv venv
+> source ./venv/bin/activate
+> pip install -r requirements.txt
+
--- python-chatgpt.py
+++ python-chatgpt.py
@@ -6,7 +6,23 @@ from dotenv import load_dotenv
 # load variables from .env file
 load_dotenv()
 
-api_endpoint = "https://api.openai.com/v1/completions"
+url = "https://api.openai.com/v1/completions"
 api_key = os.getenv("OPENAI_API_KEY")
 
-print(api_key)
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer {}".format(api_key)
+    }
+
+data = {
+    "model": "text-davinci-003",
+    "prompt":"Write a pytest test for post request for fastAPI",
+    "max_tokens": 2000,
+    "temperature": 0.3
+}
+
+# Send a POST request to the API with the data included
+response = requests.post(url, json=data, headers=headers)
+
+if response.status_code == 200:
+    print(response.json()["choices"][0]["text"])
